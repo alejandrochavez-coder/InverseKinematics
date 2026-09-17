cf2d7980ffff47039136af877f8c70a03b4c7f48
alejandrochavez-coder <alejandro.chavez@utec.edu.pe>
2026-09-17 13:11:10 -0500

    ecs no work
    pepe

--- main.cpp
+++ main.cpp
@@ -1,84 +1,54 @@
-// #include <iostream>
-// #include "vector2d.h"
-// #include "kinematics.h"
-
-// Vector2D request_vector(const std::string& prompt = "Enter vector x, y: ") {
-//     double x, y;
-//     std::cout << prompt;
-//     std::cin >> x >> y;
-
-//     return Vector2D(x, y);
-// }
-
-// void print_vector(const Vector2D& vector) {
-//     std::cout << vector.x << ", " << vector.y << std::endl;
-// }
-
-// bool request_exit(const std::string& prompt = "Want to exit? : ") {
-//     bool x;
-//     std::cout << prompt;
-//     std::cin >> x;
-//     return !x;
-// }
-
-// int main() {
-//     auto anchor = Vector2D{0, 0};
-
-//     IKInstance ik_instance = kin_create_inverse_instance(Vector2D{0, 0});
-//     kin_inverse_add_segment(ik_instance, Vector2D{1, 0}, 5);
-//     kin_inverse_add_segment(ik_instance, Vector2D{0, 1}, 5);
-
-//     while (true) {
-//         Vector2D requested_target = request_vector();
-//         kin_inverse_update(ik_instance, requested_target);
-
-//         print_vector(kin_inverse_tip_position(ik_instance));
-//         for (auto position : kin_inverse_enumerate_segments(ik_instance)) {
-//             print_vector(position.root_position);
-//         }
-//     }
-
-//     kin_destroy_inverse_instance(ik_instance);
-//     return 0;
-// }
-
 #include <unordered_map>
 #include <vector>
 #include <cmath>
 #include <iostream>
 #include <variant>
-#include <raylib.h>
-#include <raymath.h>
 #include "vector2d.h"
+#include <typeindex>
+#include <set>
 
 struct Segment {
 	Vector2D start;
 	Vector2D end;
 };
 
-struct Physics {
-	Vector2D gravity;
+struct Container {
+	int id;
 };
 
-struct Drawable {};
+struct ContainerManager {
+	std::unordered_map<std::type_index, void*> a;
+};
 
-static void draw_segment(const Segment& segment) {
-	Vector2 anchor = {
-		GetScreenWidth() / 2.0,
-		GetScreenHeight() / 2.0
-	};
+template <typename... Components>
+struct Archeotype {
 
-	Vector2 start = { static_cast<float>(segment.start.x), static_cast<float>(segment.start.y) };
-	Vector2 end = { static_cast<float>(segment.end.x), static_cast<float>(segment.end.y) };
-	start = Vector2Add(start, anchor);
-	end = Vector2Add(end, anchor);
+};
 
-	DrawLineEx(start, end, 2, BLACK);
+template <typename T>
+void container_add_component(ContainerManager& manager, T component) {
+	
+}
+
+template <typename... Queried>
+Archeotype<Queried...> manager_get_archeotype(Queried... queries) {
+
+}
+
+
+
+template <typename... Queried>
+std::vector<std::tuple<Queried...>> container_query(ContainerManager& manager) {
+	std::vector<std::tuple<Queried...>> data{};
+	Archeotype archeotype = manager_get_archeotype(Queried...);
 }
 
 int main() {
-	InitWindow(800, 600, "Kinematics Simulation");
+	ContainerManager manager{};
+
+
     std::vector<Segment> segments;
+
 
     Segment first{};
 	first.start = Vector2D{0, 0};
@@ -86,20 +56,15 @@ int main() {
 
     segments.push_back(first);
 
-	while (true) {
-		if (WindowShouldClose()) {
-			CloseWindow();
-			break;
-		}
+	for (const auto& [segment] : container_query<Segment>(manager)) {
+		
+	}
 
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
+	while (true) {
 
 		for (const auto& segment : segments) {
-			draw_segment(segment);
+			std::cout << "PEPE";
 		}
-
-		EndDrawing();
 	}
 
     return 0;
